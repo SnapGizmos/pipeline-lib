111209fd508f0fe853f4f9fefd53a6222be1315b
Tite <ego@tite.as>
2016-12-11 19:22:46 -0500

    initial commit

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -60,7 +60,7 @@ def origin(body) {
         stage('Deploy DEV') {
             sh "oc get pods -n ${config.namespace}"
             sh "oc projects"
-            sh "export ARTIFACT_URL=http://nexus-poclab.h.svc.tite.lan/service/local/artifact/maven/redirect?r=snapshots&g=${group()}&a=${artifact()}&v=${version()}"
+            sh "export ARTIFACT_URL=http://nexus-poclab.h.svc.tite.lan/service/local/artifact/maven/redirect?r=snapshots\&g=${group()}\&a=${artifact()}\&v=${version()}"
             sh "bin/render-template.sh ${config.namespace}"
             sh "rm -rf oc-build && mkdir -p oc-build/deployments"
             sh "cp target/openshift-tasks.war oc-build/deployments/ROOT.war"
