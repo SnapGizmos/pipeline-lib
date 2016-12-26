3e6d69c8034a5dab4a7212c1d51b3a4d889a82a5
Tite <ego@tite.as>
2016-12-26 10:43:26 -0500

    void

--- src/com/snapgizmos/cicd/OpenshiftHelper.groovy
+++ src/com/snapgizmos/cicd/OpenshiftHelper.groovy
@@ -53,8 +53,10 @@ class OpenshiftHelper implements Serializable {
 //            def rawParams = script.sh script: "oc process --parameters -n ${this.config.namespace} ${tmplName} | grep -oh '^\\w*' | grep -v '^NAME\$')", returnStdout: true
             script.echo "oc process --parameters -n ${this.config.namespace} ${tmplName} | grep -oh '^\\w*' | grep -v '^NAME\$'"
             script.echo "Raw template is ${tmplName}"
-            def rawParams = script.sh script: "oc process --parameters -n ${this.config.namespace} ${tmplName} ", returnStdout: true
-            script.echo "Raw params is ${rawParams}"
+            script.sh script: "oc process --parameters -n ${this.config.namespace} "
+            script.sh script: "oc process --parameters -n ${this.config.namespace} ${tmplName} "
+//            def rawParams = script.sh script: "oc process --parameters -n ${this.config.namespace} ${tmplName} ", returnStdout: true
+//            script.echo "Raw params is ${rawParams}"
         } catch (Exception e) {
             script.echo "Silengly ignoring _expected_ exception .. "
             script.echo "toString ${e.toString()} "
