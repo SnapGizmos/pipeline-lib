07445c9c081065fe591249138a1eff80a758c888
Tite <ego@tite.as>
2016-12-26 08:24:53 -0500

    void

---
+++ src/com/snapgizmos/cicd/OpenshiftHelper.groovy
@@ -0,0 +1,80 @@
+package com.snapgizmos.cicd
+
+@Grapes(
+        @Grab('org.yaml:snakeyaml:1.17')
+)
+@Grapes(
+        @Grab(group='org.json', module='json', version='20160810')
+)
+
+import org.json.JSONObject;
+import org.yaml.snakeyaml.Yaml
+
+/**
+ * Created by tite on 12/25/16.
+ */
+class OpenshiftHelper implements Serializable {
+    private Script script = null;
+    private Map config = null;
+//    private Map config = new HashMap();
+
+//    @NonCPS
+    def OpenshiftHelper(Script script, Map config) {
+        println "echo TITE: OpenshiftHelper constructur!!"
+        this.script = script
+        this.config = config
+        println "TITE: OpenshiftHelper constructur!! /end "
+    }
+
+    def testJson() {
+        script.echo "OpenshiftHelper.testJson() Config is actually '${config}' "
+        def strFile = script.readFile file: "openshift/templates/test.json"
+        def jsonArray = new JSONObject( strFile )
+        script.echo jsonArray
+    }
+
+    def processTemplate(def tname) {
+    println "Config is actually ${this.config}"
+    script.echo "Config is actually '${this.config}' "
+    def strFile = script.readFile file: "openshift/templates/${this.config.tmplOpenshift}"
+//    script.echo strFile
+//    def is = new ByteArrayInputStream(strFile.getBytes(StandardCharsets.UTF_8))
+//    def is = new File(baseDir,'openshift/templates/config-server-javase.yaml').newInputStream()
+    script.sh "echo TITE1 "
+    println "TITE1 "
+
+    try {
+        script.openshiftCreateResource jsonyaml: strFile, namespace: 'dev', verbose: 'false'
+    } catch (Exception e) {
+        script.echo "Silengly ignoring exception : "
+//        script.echo e.getStackTrace()
+        script.echo e.toString()
+    }
+
+    try {
+        Yaml templateYml = new Yaml()
+        script.echo "TITE2 what a bitch! "
+        def yamlParser = templateYml.load(strFile)
+        script.echo "TITE3 holly moses! "
+        def j = yamlParser.get('objects').size()
+        script.echo "template is ${yamlParser.getClass()}"
+        for (int i=0; i< j; i++) {
+            script.echo "Iterating ... "
+            script.echo "Iterating over ${yamlParser['objects'][i]} "
+//            script.sh "echo oc delete ${itm['kind']}/${itm['metadata']['name']} -n poclab "
+        }
+    } catch (Exception e) {
+        script.echo "Silengly ignoring _expected_ exception .. "
+        script.echo e.toString()
+        script.echo e.getMessage()
+    }
+    /** **
+//        def proc = "oc delete ${itm['kind']}/${itm['metadata']['name']} -n poclab ".execute()
+//        def outputStream = new StringBuffer()
+//        proc.waitForProcessOutput(outputStream,System.err)
+//        println outputStream.toString()
+//    is.close()
+     /** **/
+}
+
+}
--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -1,81 +1,6 @@
 #!groovy
 
-@Grab('org.yaml:snakeyaml:1.17')
-
-
-import org.yaml.snakeyaml.Yaml
-import java.nio.charset.StandardCharsets
-import com.snapgizmos.cicd.OpenshiftCLI;
-
-def static paramTest(Script script,def parser) {
-    script.echo " class is ${parser.getClass()}"
-}
-
-def static renderTemplate(Script script,def config) {
-//def static renderTemplate(String fname) {
-//    def baseDir = '.'
-    println "Config is actually ${config}"
-    script.echo "Config is actually '${config}' "
-    def strFile = script.readFile file: "openshift/templates/${config.tmplOpenshift}"
-//    script.echo strFile
-//    def is = new ByteArrayInputStream(strFile.getBytes(StandardCharsets.UTF_8))
-//    def is = new File(baseDir,'openshift/templates/config-server-javase.yaml').newInputStream()
-    script.sh "echo TITE1 "
-    println "TITE1 "
-
-    try {
-        script.openshiftCreateResource jsonyaml: strFile, namespace: 'dev', verbose: 'false'
-    } catch (Exception e) {
-        script.echo "Silengly ignoring exception : "
-//        script.echo e.getStackTrace()
-        script.echo e.toString()
-    }
-
-    try {
-        Yaml templateYml = new Yaml()
-        script.echo "TITE2 what a bitch! "
-        def yamlParser = templateYml.load(strFile)
-        script.echo "TITE3 holly moses! "
-        def j = yamlParser.get('objects').size()
-        script.echo "template is ${yamlParser.getClass()}"
-        for (int i=0; i< j; i++) {
-            script.echo "Iterating ... "
-            script.echo "Iterating over ${yamlParser['objects'][i]} "
-//            script.sh "echo oc delete ${itm['kind']}/${itm['metadata']['name']} -n poclab "
-        }
-    } catch (Exception e) {
-        script.echo "Silengly ignoring _expected_ exception .. "
-        script.echo e.toString()
-        script.echo e.getMessage()
-    }
-    /** **
-//        def proc = "oc delete ${itm['kind']}/${itm['metadata']['name']} -n poclab ".execute()
-//        def outputStream = new StringBuffer()
-//        proc.waitForProcessOutput(outputStream,System.err)
-//        println outputStream.toString()
-//    is.close()
-     /** **/
-}
-
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    node {
-        stage('Checkout') {
-            checkout scm
-        }
-        stage('Main') {
-            docker.image(config.environment).inside {
-                sh config.mainScript
-            }
-        }
-        stage('Post') {
-            sh config.postScript
-        }
-    }
-}
+import com.snapgizmos.cicd.OpenshiftHelper;
 
 def origin(body) {
     def config = [:]
@@ -142,42 +67,31 @@ def origin(body) {
             sh "cat $WORKSPACE/openshift/env "
 
             try {
-//                def oscli = new OpenshiftCLI('this','config');
-                def oscli = new OpenshiftCLI(this,config);
-//            def oscli = new OpenshiftCLI();
+//                def oscli = new OpenshiftHelper('this','config');
+                def oscli = new OpenshiftHelper(this,config);
+//            def oscli = new OpenshiftHelper();
                 println "openshift cli is : ${oscli}"
-                oscli.renderTemplate()
+                oscli.testJson()
             } catch (Exception e) {
                 println e.toString()
             }
-//            renderTemplate(config.tmplOpenshift)
+//            testJson(config.tmplOpenshift)
 //            sh "bin/render-template.sh ${config.namespace}"
             def strFile = readFile file: "openshift/templates/${config.tmplOpenshift}"
             println "files has: ${strFile.length()}"
 
-            renderTemplate(this,config)
+            try {
+                oscli.processTemplate(config.tmplOpenshift)
+//            testJson(config.tmplOpenshift)
+            } catch (Exception e) {
+                println e.toString()
+            }
 
-            echo "called local renderTemplate. Testing the bad apple"
+            echo "called local testJson. Testing the bad apple"
             Yaml templateYml = new Yaml()
             def yamlParser = templateYml.load(strFile)
             echo "template is ${yamlParser.getClass()}"
-            paramTest(this,yamlParser)
-            echo "YESSSSS "
 
-
-            /** old crap **
-            sh "rm -rf oc-build && mkdir -p oc-build/deployments"
-            sh "cp target/openshift-tasks.war oc-build/deployments/ROOT.war"
-            // clean up. keep the image stream
-            sh "${ocCmd} delete bc,dc,svc,route -l app=tasks -n dev"
-            // create build. override the exit code since it complains about exising imagestream
-            sh "${ocCmd} new-build --name=tasks --image-stream=jboss-eap70-openshift --binary=true --labels=app=tasks -n dev || true"
-            // build image
-            sh "${ocCmd} start-build tasks --from-dir=oc-build --wait=true -n dev"
-            // deploy image
-            sh "${ocCmd} new-app tasks:latest -n dev"
-            sh "${ocCmd} expose svc/tasks -n dev"
-             /** **/
         }
 
         stage('Deploy STAGE') {
@@ -207,68 +121,3 @@ def group() {
     matcher ? matcher[0][1] : null
 }
 
-def origin_orig() {
-    node('maven') {
-        sh "env"
-
-        // define commands
-        def ocCmd = "oc --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` --server=https://osmaster.mgt.tite.lan:8443 --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-        // def mvnCmd = "mvn -s configuration/cicd-settings.xml"
-        def mvnCmd = "mvn -s settings.xml"
-
-        new File(".").eachFile() {
-            file->println file.getAbsolutePath()
-        }
-
-        stage 'Build'
-        git branch: 'master', url: 'http://gogs:3000/gogs/config-server-poc.git'
-        def v = version()
-        sh "${mvnCmd} clean install -DskipTests=true"
-
-        stage 'Test and Analysis'
-        parallel (
-                'Test': {
-                    sh "${mvnCmd} test"
-                    step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-                }
-                ,
-                'Static Analysis': {
-                    sh "${mvnCmd} jacoco:report sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -DskipTests=true"
-                }
-        )
-
-        stage 'Push to Nexus'
-        //configFileProvider([configFile(fileId: '00045d94-5f1a-4647-96d2-1172f422be0a', targetLocation: 'settings.xml', variable: 'NEXUS_SETTINGS')]) {
-        //// some block
-        //sh "${mvnCmd} -s $NEXUS_SETTINGS deploy -DskipTests=true"
-        //}
-
-        stage 'Deploy DEV'
-        sh "alias oc=${ocCmd}"
-        sh "bin/render-template.sh dev"
-        sh "alias oc=oc"
-        sh "rm -rf oc-build && mkdir -p oc-build/deployments"
-        sh "cp target/openshift-tasks.war oc-build/deployments/ROOT.war"
-        // clean up. keep the image stream
-        sh "${ocCmd} delete bc,dc,svc,route -l app=tasks -n dev"
-        // create build. override the exit code since it complains about exising imagestream
-        sh "${ocCmd} new-build --name=tasks --image-stream=jboss-eap70-openshift --binary=true --labels=app=tasks -n dev || true"
-        // build image
-        sh "${ocCmd} start-build tasks --from-dir=oc-build --wait=true -n dev"
-        // deploy image
-        sh "${ocCmd} new-app tasks:latest -n dev"
-        sh "${ocCmd} expose svc/tasks -n dev"
-
-        stage 'Deploy STAGE'
-        input message: "Promote to STAGE?", ok: "Promote"
-        // tag for stage
-        sh "${ocCmd} tag dev/tasks:latest stage/tasks:${v}"
-        // clean up. keep the imagestream
-        sh "${ocCmd} delete bc,dc,svc,route -l app=tasks -n stage"
-        // deploy stage image
-        sh "${ocCmd} new-app tasks:${v} -n stage"
-        sh "${ocCmd} expose svc/tasks -n stage"
-    }
-}
-
-return this
