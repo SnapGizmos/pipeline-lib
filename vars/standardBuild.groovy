069bc1dd3585c2a93cc91fafef091921cfb1bf50
Tite <ego@tite.as>
2017-01-08 10:11:19 -0500

    added accidentally deleted test and analysis stage

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -12,7 +12,7 @@ def origin(body) {
 
 //    config.environment.each { k, v -> println "out: going over ${k}=${v} " }
     if (!config.targetStages) {
-        config.targetStages = ['Build','Test and Analysis','Push to Nexus','Deploy DEV','Deploy STAGE']
+        config.targetStages = ['Build', 'Test and Analysis', 'Push to Nexus', 'Deploy DEV', 'Deploy STAGE']
     }
 
     if (true || 'Build' in config.targetStages) config.nodetype = 'maven'
@@ -39,51 +39,65 @@ def origin(body) {
                 sh "${mvnCmd} clean install -DskipTests=true"
             }
 
+        if ('Test and Analysis' in config.targetStages)
+            stage 'Test and Analysis' {
+                parallel(
+                        'Test': {
+                            sh "${mvnCmd} test"
+                            step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+                        }
+                        ,
+                        'Static Analysis': {
+                            sh "${mvnCmd} jacoco:report sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -DskipTests=true"
+                        }
+                )
+            }
+
         if ('Push to Nexus' in config.targetStages)
-        stage('Push to Nexus') {
-            //configFileProvider([configFile(fileId: '00045d94-5f1a-4647-96d2-1172f422be0a', targetLocation: 'settings.xml', variable: 'NEXUS_SETTINGS')]) {
-            //// some block
-            sh "${mvnCmd} deploy -DskipTests=true"
-        }
+            stage('Push to Nexus') {
+                //configFileProvider([configFile(fileId: '00045d94-5f1a-4647-96d2-1172f422be0a', targetLocation: 'settings.xml', variable: 'NEXUS_SETTINGS')]) {
+                //// some block
+                sh "${mvnCmd} deploy -DskipTests=true"
+            }
 
         if ('Deploy DEV' in config.targetStages)
-        stage('Deploy DEV') {
-            sh "oc get pods -n ${config.namespace}"
-            sh "oc get projects -n ${config.namespace}"
-            sh "oc project ${config.namespace}"
-            sh "oc get pods -n ${config.namespace}"
+            stage('Deploy DEV') {
+                sh "oc get pods -n ${config.namespace}"
+                sh "oc get projects -n ${config.namespace}"
+                sh "oc project ${config.namespace}"
+                sh "oc get pods -n ${config.namespace}"
 //            sh "env"
-            /** **
-            Maybe the ARTIFACT_URL could be rendered based on pom.xml
-             nexus.h.svc.tite.lan/service/local/artifact/maven/redirect?r=snapshots\&g=${group()}\&a=${artifact()}\&v=${version()}"
-             /** **/
+                /** **
+                 Maybe the ARTIFACT_URL could be rendered based on pom.xml
+                 nexus.h.svc.tite.lan/service/local/artifact/maven/redirect?r=snapshots\&g=${group()}\&a=${artifact()}\&v=${version()}"
+                 /** **/
 
-            def oscli
-            try {
-                oscli = new OpenshiftHelper(this,config);
-                println "openshift cli is : ${oscli}"
+                def oscli
+                try {
+                    oscli = new OpenshiftHelper(this, config);
+                    println "openshift cli is : ${oscli}"
 //                oscli.testJson()
-            } catch (Exception e) {
-                println e.toString()
-            }
+                } catch (Exception e) {
+                    println e.toString()
+                }
 //            testJson(config.tmplOpenshift)
 //            sh "bin/render-template.sh ${config.namespace}"
-            def strFile = readFile file: "openshift/templates/${config.tmplOpenshift}"
-            println "BEFORE helper files has: ${strFile.length()}"
+                def strFile = readFile file: "openshift/templates/${config.tmplOpenshift}"
+                println "BEFORE helper files has: ${strFile.length()}"
 
-            try {
-                oscli.processTemplate(config.tmplOpenshift)
+                try {
+                    oscli.processTemplate(config.tmplOpenshift)
 //            testJson(config.tmplOpenshift)
-            } catch (Exception e) {
-                println e.toString()
-            }
+                } catch (Exception e) {
+                    println e.toString()
+                }
 
 //            echo "called local testJson. Testing the bad apple"
 //            Yaml templateYml = new Yaml()
 //            def yamlParser = templateYml.load(strFile)
 //            echo "template is ${yamlParser.getClass()}"
 
-        }
+            }
 
         stage('Deploy STAGE') {
             input message: "Promote to STAGE?", ok: "Promote"
@@ -103,10 +117,12 @@ def version() {
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
     matcher ? matcher[0][1] : null
 }
+
 def artifact() {
     def matcher = readFile('pom.xml') =~ '<artifactId>(.+)</artifactId>'
     matcher ? matcher[0][1] : null
 }
+
 def group() {
     def matcher = readFile('pom.xml') =~ '<groupId>(.+)</groupId>'
     matcher ? matcher[0][1] : null
