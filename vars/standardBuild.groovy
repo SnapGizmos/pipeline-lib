fb8a7759d0d6575b6ef8cc8f6d528f6d50728bf3
Tite <ego@tite.as>
2017-01-08 17:33:39 -0500

    added jacoco coverage to sonarqube

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -48,7 +48,7 @@ def origin(body) {
                         }
                         ,
                         'Static Analysis': {
-                            sh "${mvnCmd} org.jacoco:jacoco-maven-plugin:prepare-agent jacoco:report sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -DskipTests=true sonar.jacoco.reportMissing.force.zero=true"
+                            sh "${mvnCmd} org.jacoco:jacoco-maven-plugin:prepare-agent jacoco:report sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -DskipTests=true -Dsonar.jacoco.reportMissing.force.zero=true"
                         }
                 )
             }
